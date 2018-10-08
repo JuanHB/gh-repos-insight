38cbbe7f36028e83fb50dd3ad5909b757a6c617b
Juan Biscaia <juanbiscaia@gmail.com>
2018-10-07 22:54:30 -0300

    Updates

---
+++ precache-manifest.2ba5e4e91dbef3fe6150c5ad591d371b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "810daf4ba99c18dbfd83",
+    "url": "/gh-repos-insight/static/js/runtime~main.810daf4b.js"
+  },
+  {
+    "revision": "8fa43bd07ba1026d7249",
+    "url": "/gh-repos-insight/static/js/main.8fa43bd0.chunk.js"
+  },
+  {
+    "revision": "c6e461784c15c1b8b3ee",
+    "url": "/gh-repos-insight/static/js/1.c6e46178.chunk.js"
+  },
+  {
+    "revision": "8fa43bd07ba1026d7249",
+    "url": "/gh-repos-insight/static/css/main.82b99c0f.chunk.css"
+  },
+  {
+    "revision": "c6e461784c15c1b8b3ee",
+    "url": "/gh-repos-insight/static/css/1.793f240f.chunk.css"
+  },
+  {
+    "revision": "9b385d3443dbb7e188d68c908eda7958",
+    "url": "/gh-repos-insight/index.html"
+  }
+];
